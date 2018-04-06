67ea25ee97e3c8f736ace910c2520075f0362079
matthesrieke <m.rieke@52north.org>
2018-04-06 10:47:24 +0200

    initial commit

---
+++ src/main/java/org/n52/spare/kicker/registry/registryserver/RegistryServerApplication.java
@@ -0,0 +1,14 @@
+package org.n52.spare.kicker.registry.registryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServerApplication.class, args);
+	}
+}
